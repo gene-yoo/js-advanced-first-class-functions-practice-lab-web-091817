46d170a67d5d082ec297d1c526fbb3a32d26bfc7
gene-yoo <gene.y.yoo@gmail.com>
2017-10-30 21:42:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function logDriverNames (drivers) {
+  drivers.map(function (driver) {
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown (drivers, hometown) {
+  matchingDrivers = drivers.filter(function (driver) {
+    return driver.hometown === hometown;
+  });
+
+  matchingDrivers.forEach(function (driver) {
+    console.log(driver.name)
+  });
+};
+
+function driversByRevenue (drivers) {
+  return [...drivers].sort(function (a,b) {
+    return a.revenue - b.revenue;
+  });
+};
+
+function driversByName (drivers) {
+  return [...drivers].sort(function (a,b) {
+    return (a.name).localeCompare(b.name);
+  });
+};
+
+function totalRevenue (drivers) {
+  return drivers.reduce(function (sum, driver) {
+    return sum + driver.revenue;
+  }, 0);
+};
+
+function averageRevenue (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
